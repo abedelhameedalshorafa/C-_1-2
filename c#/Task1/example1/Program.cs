99f75e25f6c1905eb29c02009b3c2c0aa5c49e92
abedelhameedalshorafa <100771067+abedelhameedalshorafa@users.noreply.github.com>
2023-11-30 17:05:37 +0300

    Add files via upload

---
+++ c#/Task1/example1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+          {
+
+            ///// 1 /////
+            
+            Console.Write("Enter a string: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("You entered: " + userInput);
+
+
+            ///// 2 /////
+            
+            double doubleVariable = 3.14;
+            string stringVariable = "Hello World!";
+            char charVariable = 'L';
+            bool boolVariable = true;
+            int intVariable = 42;
+            const int constantVariable = 100; 
+
+            Console.WriteLine("Double Variable: " + doubleVariable);
+            Console.WriteLine("String Variable: " + stringVariable);
+            Console.WriteLine("Char Variable: " + charVariable);
+            Console.WriteLine("Bool Variable: " + boolVariable);
+            Console.WriteLine("Int Variable: " + intVariable);
+            Console.WriteLine("Constant Variable: " + constantVariable);
+
+
+            ///// 3 /////
+
+            string[] cars = { "Toyota", "Honda", "Ford"};
+            Console.WriteLine("List of Cars:");
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+            Console.WriteLine("Number of Cars: " + cars.Length);
+
+
+            ///// 4 /////
+
+            string firstName, lastName;
+            int yearOfBirth;
+
+            Console.Write("Input your firstname: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Input your lastname: ");
+            lastName = Console.ReadLine();
+
+            Console.Write("Input your year of birth: ");
+            
+            while (!int.TryParse(Console.ReadLine(), out yearOfBirth))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid year of birth.");
+                Console.Write("Input your year of birth: ");
+            }
+
+            Console.WriteLine($"{firstName} {lastName} {yearOfBirth}");
+
+            ///// 5 /////
+
+            int[] elements = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Enter element - {i}: ");
+
+                while (!int.TryParse(Console.ReadLine(), out elements[i]))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                    Console.Write($"Enter element - {i}: ");
+                }
+            }
+
+            Console.Write("Elements in array are: ");
+            foreach (int element in elements)
+            {
+                Console.Write(element + " ");
+            }
+
+
+            ///// 6 /////
+
+            int[] numbers = { 1, 2, 3, 4 };
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Enter element - {i}: ");
+
+                while (!int.TryParse(Console.ReadLine(), out numbers[i]))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                    Console.Write($"Enter element - {i}: ");
+                }
+            }
+
+            Console.WriteLine("Sum of all elements stored in the array is :" + numbers.Sum());
+
+        }
+    }
+}
---
+++ c#/Task2/Task2/Program.cs
@@ -0,0 +1,237 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///// 1 /////
+
+            //Console.Write("Enter the first integer: ");
+
+            //if (!int.TryParse(Console.ReadLine(), out int firstNumber))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //    return;
+            //}
+
+            
+            //Console.Write("Enter the second integer: ");
+
+            //if (!int.TryParse(Console.ReadLine(), out int secondNumber))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //    return;
+            //}
+
+            //int smallerNumber = Math.Min(firstNumber, secondNumber);
+
+            //Console.WriteLine("The smaller number is: " + smallerNumber);
+
+
+            /////// 2 /////
+
+            //Console.Write("Enter a number: ");
+
+            //if (!int.TryParse(Console.ReadLine(), out int number))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //    return;
+            //}
+
+            //string sign = (number > 0) ? "-" : (number < 0) ? "+" : "zero";
+
+            //Console.WriteLine($"The sign is {sign}");
+
+
+            ///// 3 /////
+
+            //Console.Write("Enter the first number: ");
+            //if (!int.TryParse(Console.ReadLine(), out int number1))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //    return;
+            //}
+
+            //Console.Write("Enter the second number: ");
+            //if (!int.TryParse(Console.ReadLine(), out int number2))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //    return;
+            //}
+
+            //Console.Write("Enter the third number: ");
+            //if (!int.TryParse(Console.ReadLine(), out int number3))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //    return;
+            //}
+            //int temp;
+            //if (number1 > number2)
+            //{
+            //    temp = number1;
+            //    number1 = number2;
+            //    number2 = temp;
+            //}
+
+            //if (number2 > number3)
+            //{
+            //    temp = number2;
+            //    number2 = number3;
+            //    number3 = temp;
+            //}
+
+            //if (number1 > number2)
+            //{
+            //    temp = number1;
+            //    number1 = number2;
+            //    number2 = temp;
+            //}
+
+            //// Display the sorted numbers
+            //Console.WriteLine($"Sorted numbers in ascending order: {number3}, {number2}, {number1}");
+
+
+            ///// 4 /////
+
+
+            Console.Write("Enter the first number: ");
+            if (!int.TryParse(Console.ReadLine(), out int number1))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                return;
+            }
+
+            Console.Write("Enter the second number: ");
+            if (!int.TryParse(Console.ReadLine(), out int number2))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                return;
+            }
+
+            Console.Write("Enter the third number: ");
+            if (!int.TryParse(Console.ReadLine(), out int number3))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                return;
+            }
+
+            Console.Write("Enter the fourth number: ");
+            if (!int.TryParse(Console.ReadLine(), out int number4))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                return;
+            }
+
+            Console.Write("Enter the fifth number: ");
+            if (!int.TryParse(Console.ReadLine(), out int number5))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                return;
+            }
+
+            int maxNumber = Math.Max(Math.Max(Math.Max(Math.Max(number1, number2), number3), number4), number5);
+
+            Console.WriteLine($"The maximum number is: {maxNumber}");
+
+
+            ///// 5 /////
+            
+            Console.Write("Input kilometers per hour: ");
+            if (!double.TryParse(Console.ReadLine(), out double kmPerHour))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                return;
+            }
+
+            const double ConversionFactor = 0.621371;
+
+            double milesPerHour = kmPerHour * ConversionFactor;
+
+            Console.WriteLine($"{kmPerHour} kilometers per hour is equal to {milesPerHour} miles per hour");
+
+
+            ///// 6 /////
+
+            //Console.Write("Input hours: ");
+            //if (!int.TryParse(Console.ReadLine(), out int hours))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer for hours.");
+            //    return;
+            //}
+
+            //Console.Write("Input minutes: ");
+            //if (!int.TryParse(Console.ReadLine(), out int minutes))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer for minutes.");
+            //    return;
+            //}
+
+            //int totalMinutes = hours * 60 + minutes;
+
+            //Console.WriteLine($"Total: {totalMinutes} minutes.");
+
+
+
+            ///// 7 /////
+
+            Console.Write("Input minutes: ");
+            if (!int.TryParse(Console.ReadLine(), out int totalMinutes))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer for minutes.");
+                return;
+            }
+
+            int hours = totalMinutes / 60;
+            int remainingMinutes = totalMinutes % 60;
+
+            Console.WriteLine($"{totalMinutes} minutes is equal to {hours} Hours, {remainingMinutes} Minutes");
+
+
+            ///// 8 /////
+            
+
+            string[] sentences = {
+            "This is a short sentence.",
+            "Programming is fun and rewarding.",
+            "C# is a versatile programming language.",
+            "Arrays help store and organize data.",
+            "Learning new things is exciting!"
+            };
+
+            Console.WriteLine("Sentences with Fixed Length:");
+            foreach (string sentence in sentences)
+            {
+                Console.WriteLine(sentence.PadRight(30)); 
+            }
+
+            Console.WriteLine(reverseOdd("Bananas")); 
+            Console.WriteLine(reverseOdd("One two three four"));
+
+
+        }
+
+        static string reverseOdd(string input)
+        {
+            string[] words = input.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length % 2 != 0)
+                {
+                    char[] charArray = words[i].ToCharArray();
+                    Array.Reverse(charArray);
+                    words[i] = new string(charArray);
+                }
+            }
+
+            
+            string result = string.Join(" ", words);
+
+            return result;
+        }
+    }
+
+
+}
